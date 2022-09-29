c274f22c55bb5d8371536259f44bc5435d840eff
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-09-29 09:02:01 +0600

    add cart break click handlar

---
+++ src/component/Cart/Cart.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import {  faLocation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Cart = ({time}) => {
+    const [cartTime , setCartTime] = useState(0)
+
+    const eventHandle =(event) =>{
+        setCartTime(event)
+    }
+
+
+    return (
+        <div className="card w-96 bg-gray-300 rounded-none h-full p-5 ">
+        <div className='flex justify-between mb-5'>
+            <div className="avatar">
+                <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <img src="https://placeimg.com/192/192/people" />
+                </div>
+            </div>
+            <div>
+                <h1 className='card-title'>Mohammad Sazzatul Islam</h1>
+                <p><FontAwesomeIcon icon ={faLocation}></FontAwesomeIcon>  Mirsarai, Chittagong</p>
+            </div>
+        </div>
+
+        <div className="stats stats-vertical lg:stats-horizontal border-none rounded-md bg-gray-100">
+            <div className="stat border-none ">
+                <h1 className="text-3xl font-bold">31<small className="font-semibold text-sm ">kg</small></h1>
+                <p className="font-semibold">weight</p>
+            </div>
+            <div className="stat border-none ">
+                <h1 className="text-3xl font-bold">31<small className="font-semibold text-sm ">kg</small></h1>
+                <p className="font-semibold">weight</p>
+            </div>
+            <div className="stat border-none ">
+                <h1 className="text-3xl font-bold">31<small className="font-semibold text-sm ">kg</small></h1>
+                <p className="font-semibold">weight</p>
+            </div>
+        </div>
+
+        <div>
+            <h1 className='card-title text-xl font-semibold '> Add A Break </h1>
+            <div className="btn-group lg:btn-group-horizontal bg-gray-200 px-3 py-4 w-full flex justify-around mt-5 rounded-md ">
+                <button className='rounded-full hover:bg-blue-700 hover:text-white focus:outline-none focus:bg-blue-700 focus:text-white bg-white px-2 py-3 text-2xs '><small className='text-2xl font-bold' onClick={(e)=> eventHandle(e.target.innerText)} >10</small>m</button>
+                <button className='rounded-full hover:bg-blue-700 hover:text-white focus:outline-none focus:bg-blue-700 focus:text-white bg-white px-2 py-3 text-2xs '><small className='text-2xl font-bold' onClick={(e)=> eventHandle(e.target.innerText)} >20</small>m</button>
+                <button className='rounded-full hover:bg-blue-700 hover:text-white focus:outline-none focus:bg-blue-700 focus:text-white bg-white px-2 py-3 text-2xs '><small className='text-2xl font-bold' onClick={(e)=> eventHandle(e.target.innerText)} >30</small>m</button>
+                <button className='rounded-full hover:bg-blue-700 hover:text-white focus:outline-none focus:bg-blue-700 focus:text-white bg-white px-2 py-3 text-2xs '><small className='text-2xl font-bold' onClick={(e)=> eventHandle(e.target.innerText)} >40</small>m</button>
+                <button className='rounded-full hover:bg-blue-700 hover:text-white focus:outline-none focus:bg-blue-700 focus:text-white bg-white px-2 py-3 text-2xs '><small className='text-2xl font-bold' onClick={(e)=> eventHandle(e.target.innerText)} >50</small>m</button>                
+            </div>
+        </div>
+
+        <div>
+            <h1 className='card-title text-xl font-semibold '> Playing Details </h1>
+            <div className='flex justify-between mt-5 py-3 bg-gray-200 px-4 rounded-md'>
+                <h1 className='card-title'>Playing time </h1>
+                <h1 className='text-xl'>{time} minutes</h1>
+            </div>
+            <div className='flex justify-between mt-5 py-3 bg-gray-200 px-4 rounded-md'>
+                <h1 className='card-title'>Break time </h1>
+                <h1 className='text-xl'> {cartTime} minutes</h1>
+            </div>
+
+            <div className="card-actions mt-16 ">
+                <button className="btn btn-primary rounded-sm w-full ">Add to list</button>
+            </div>
+
+        </div>
+    </div>
+    );
+};
+
+export default Cart;
--- src/component/Players/Players.js
+++ src/component/Players/Players.js
@@ -1,6 +1,7 @@
-import { faBasketball, faLocation } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
+import { faBasketball } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Cart from '../Cart/Cart';
 import Player from '../Player/Player';
 
 const Players = () => {
@@ -38,63 +39,9 @@ const Players = () => {
             </div>
 
             <div>
-               
-                <div className="card w-96 bg-gray-300 rounded-none h-full p-5  ">
-                    <div className='flex justify-between mb-5'>
-                        <div className="avatar">
-                            <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                <img src="https://placeimg.com/192/192/people" />
-                            </div>
-                        </div>
-                        <div>
-                            <h1 className='card-title'>Mohammad Sazzatul Islam</h1>
-                            <p><FontAwesomeIcon icon ={faLocation}></FontAwesomeIcon>  Mirsarai, Chittagong</p>
-                        </div>
-                    </div>
-
-                    <div className="stats stats-vertical lg:stats-horizontal border-none rounded-md bg-gray-100">
-                        <div className="stat border-none ">
-                            <h1 className="text-3xl font-bold">31<small className="font-semibold text-sm ">kg</small></h1>
-                            <p className="font-semibold">weight</p>
-                        </div>
-                        <div className="stat border-none ">
-                            <h1 className="text-3xl font-bold">31<small className="font-semibold text-sm ">kg</small></h1>
-                            <p className="font-semibold">weight</p>
-                        </div>
-                        <div className="stat border-none ">
-                            <h1 className="text-3xl font-bold">31<small className="font-semibold text-sm ">kg</small></h1>
-                            <p className="font-semibold">weight</p>
-                        </div>
-                    </div>
-
-                    <div>
-                        <h1 className='card-title text-xl font-semibold '> Add A Break </h1>
-                        <div className="btn-group lg:btn-group-horizontal bg-gray-200 px-3 py-4 w-full flex justify-around mt-5 rounded-md ">
-                            <button className='rounded-full bg-white p-3 '>20s</button>
-                            <button className='rounded-full bg-white p-3 '>20s</button>
-                            <button className='rounded-full bg-white p-3 '>20s</button>
-                            <button className='rounded-full bg-white p-3 '>20s</button>
-                            <button className='rounded-full bg-white p-3 '>20s</button>
-                        </div>
-                    </div>
-
-                    <div>
-                        <h1 className='card-title text-xl font-semibold '> Playing Details </h1>
-                        <div className='flex justify-between mt-5 py-3 bg-gray-200 px-4 rounded-md'>
-                            <h1 className='card-title'>Playing time </h1>
-                            <h1 className='text-xl'>{addCartTime} minutes</h1>
-                        </div>
-                        <div className='flex justify-between mt-5 py-3 bg-gray-200 px-4 rounded-md'>
-                            <h1 className='card-title'>Break time </h1>
-                            <h1 className='text-xl'> {} minutes</h1>
-                        </div>
-
-                        <div className="card-actions mt-16 ">
-                            <button className="btn btn-primary rounded-sm w-full ">Add to list</button>
-                        </div>
-
-                    </div>
-                </div>
+                {
+                    <Cart time = {addCartTime} ></Cart>
+                }
             </div>
         </div>
     );
